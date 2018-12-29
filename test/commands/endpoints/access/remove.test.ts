b1ab6f9150c0362eb85c656d14d20e01d5b61847
Brett Goulder <brett.goulder@gmail.com>
2018-12-28 16:19:54 -0800

    fix test

--- test/commands/endpoints/access/remove.test.ts
+++ test/commands/endpoints/access/remove.test.ts
@@ -1,15 +1,6 @@
 import {expect, test} from '../../../test'
 
 describe('endpoints:access:remove', () => {
-  const privateLinkWhitelistResponse = {
-    app: {name: 'myapp'},
-    addon: {name: 'postgres-123'},
-    status: 'Operational',
-    service_name: 'com.amazonaws.vpce.testvpc"',
-    connections: [],
-    whitelisted_accounts: []
-  }
-
   test
     .nock('https://postgres-api.heroku.com', api => api
       .patch('/private-link/v0/databases/postgres-123/whitelisted_accounts')

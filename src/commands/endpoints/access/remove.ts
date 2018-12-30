4a521be44c34bbac53c63c32bcc5b72c759eb156
Brett Goulder <brett.goulder@gmail.com>
2018-12-30 19:21:46 +0900

    Initial pass at the addons fetcher

--- src/commands/endpoints/access/remove.ts
+++ src/commands/endpoints/access/remove.ts
@@ -2,31 +2,33 @@ import {flags} from '@heroku-cli/command'
 import {cli} from 'cli-ux'
 
 import BaseCommand from '../../../base'
+import fetcher from '../../../lib/fetcher'
 
 export default class EndpointsAccessRemove extends BaseCommand {
   static description = 'remove an account from your Trusted VPC Endpoint\'s whitelist'
 
   static args = [
-    {name: 'database', required: true},
-    {name: 'account_ids', required: true}
+    {name: 'database'},
   ]
 
   static flags = {
+    account_ids: flags.string({required: true}),
     app: flags.app({required: true})
   }
 
   static examples = [
-    '$ heroku endpoints:access:remove arn:aws:iam::12345678910:root',
+    '$ heroku endpoints:access:remove --account_ids arn:aws:iam::12345678910:root',
   ]
 
   async run() {
-    const {args} = this.parse(EndpointsAccessRemove)
+    const {args, flags} = this.parse(EndpointsAccessRemove)
+    const database = args.database || await fetcher(this.heroku, flags.app)
 
     cli.action.start('Removing account from the whitelist')
-    await this.heroku.patch<any>(`/private-link/v0/databases/${args.database}/whitelisted_accounts`, {
+    await this.heroku.patch<any>(`/private-link/v0/databases/${database}/whitelisted_accounts`, {
       ...this.heroku.defaults,
       body: {
-        whitelisted_accounts: [args.account_ids]
+        whitelisted_accounts: [flags.account_ids]
       }
     })
     cli.action.stop()
--- src/commands/endpoints/destroy.ts
+++ src/commands/endpoints/destroy.ts
@@ -2,12 +2,13 @@ import {flags} from '@heroku-cli/command'
 import {cli} from 'cli-ux'
 
 import BaseCommand from '../../base'
+import fetcher from '../../lib/fetcher'
 
 export default class EndpointsDestroy extends BaseCommand {
   static description = 'destroy a Trusted VPC Endpoint for your database'
 
   static args = [
-    {name: 'database', required: true}
+    {name: 'database'}
   ]
 
   static flags = {
@@ -19,10 +20,11 @@ export default class EndpointsDestroy extends BaseCommand {
   ]
 
   async run() {
-    const {args} = this.parse(EndpointsDestroy)
+    const {args, flags} = this.parse(EndpointsDestroy)
+    const database = args.database || await fetcher(this.heroku, flags.app)
 
     cli.action.start('Destroying Trusted VPC Endpoint')
-    await this.heroku.delete<any>(`/private-link/v0/databases/${args.database}`, this.heroku.defaults)
+    await this.heroku.delete<any>(`/private-link/v0/databases/${database}`, this.heroku.defaults)
     cli.action.stop()
   }
 }

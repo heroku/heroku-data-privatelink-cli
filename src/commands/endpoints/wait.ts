def2700ecdcc996fccef7905c71982be4a990463
Brett Goulder <brett.goulder@gmail.com>
2018-12-30 20:50:15 +0900

    Fix endpoints wait command

--- src/commands/endpoints/wait.ts
+++ src/commands/endpoints/wait.ts
@@ -2,6 +2,7 @@ import {flags} from '@heroku-cli/command'
 import {cli} from 'cli-ux'
 
 import BaseCommand from '../../base'
+import fetcher from '../../lib/fetcher'
 
 export default class EndpointsWait extends BaseCommand {
   static description = 'wait for your Trusted VPC Endpoint to be provisioned'
@@ -19,12 +20,13 @@ export default class EndpointsWait extends BaseCommand {
   ]
 
   async run() {
-    const {args} = this.parse(EndpointsWait)
+    const {args, flags} = this.parse(EndpointsWait)
+    const database = args.database || await fetcher(this.heroku, flags.app)
 
     let status
     cli.action.start('Waiting for the Trusted VPC Endpoint to be provisioned')
     while (status !== 'Operational') {
-      let {body: res} = await this.heroku.get<any>(`/private-link/v0/databases/${args.database}`, this.heroku.defaults)
+      let {body: res} = await this.heroku.get<any>(`/private-link/v0/databases/${database}`, this.heroku.defaults)
       status = res.status
       await cli.wait(3000)
     }

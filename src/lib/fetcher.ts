d24e9748645724b662fb86ffb2736c51dd904648
Brett Goulder <brett.goulder@gmail.com>
2019-04-02 15:05:30 -0700

    Allow the CLI to work with staging addons, e.g. heroku-postgresql-justin

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,11 +2,10 @@ import {cli} from 'cli-ux'
 
 export default async function (heroku: any, addon_attachment: string, app: string) {
   const db = addon_attachment || 'DATABASE_URL'
-  const {body: res} = await heroku.post('/actions/addon-attachments/resolve', {
+  let {body: res} = await heroku.post('/actions/addon-attachments/resolve', {
     body: {
       app,
-      addon_attachment: db,
-      addon_service: process.env.HEROKU_ADDON_SERVICE || 'heroku-postgresql'
+      addon_attachment: db
     }}
   )
 

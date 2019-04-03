459b70c7dad9e3bfcc0ede2a669b6b899bc928cf
Brett Goulder <brett.goulder@gmail.com>
2019-04-03 11:45:12 -0700

    Update fetcher to use the addons resolve endpoint instead

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -2,16 +2,22 @@ import {cli} from 'cli-ux'
 
 export default async function (heroku: any, addon_attachment: string, app: string) {
   const db = addon_attachment || 'DATABASE_URL'
-  const {body: res} = await heroku.post('/actions/addon-attachments/resolve', {
+  const {body: res} = await heroku.post('/actions/addons/resolve', {
     body: {
       app,
-      addon_attachment: db
+      addon: db
     }}
   )
 
-  if (res.id !== 'not_found') {
-    return res[0].addon.name
-  } else {
+  if (res.id === 'not_found') {
     cli.error(res.message)
+  }
+
+  const filteredDb = res.find((addon: any) => addon.addon_service.name.startsWith('heroku-postgresql'))
+
+  if (filteredDb) {
+    return filteredDb.name
+  } else {
+    cli.error('No databases found')
   }
 }

a035b6161b28a04f5ba99a4c591467659bcb15a6
Brett Goulder <brett.goulder@gmail.com>
2019-04-05 11:07:24 -0700

    Satisfy typescript

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -14,7 +14,11 @@ export default async function (heroku: any, addon_attachment: string, app: strin
     cli.error(res.message)
   }
 
-  const filteredDb = res.find((addon: AddOn) => addon.addon_service && addon.addon_server.name.startsWith('heroku-postgresql'))
+  const filteredDb = res.find((addon: AddOn) => {
+    if (addon.addon_service) {
+      return addon.addon_service.name && addon.addon_service.name.startsWith('heroku-postgresql')
+    }
+  })
 
   if (filteredDb) {
     return filteredDb.name
